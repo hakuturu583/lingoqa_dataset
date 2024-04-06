257c55884a92fb6f8235d62e4983cac17a3d2790
Masaya Kataoka <ms.kataoka@gmail.com>
2024-04-07 00:01:43 +0900

    enable download from gdrive
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- lingoqa_dataset/lingoqa_dataset.py
+++ lingoqa_dataset/lingoqa_dataset.py
@@ -3,6 +3,7 @@ from abc import ABC
 from enum import Enum
 from pathlib import Path
 
+import gdown
 from torch.utils.data import Dataset
 
 
@@ -10,21 +11,25 @@ class DatasetInfo(ABC):
     gdown_object_id: str
     zip_filename: str = "images.zip"
     parquet_filename: str
+    save_directory: str
 
 
 class SceneryDatasetInfo(DatasetInfo):
     gdown_object_id = "1GiwWGfrM8pO27CYLu_9Uwtdcz0JoqHr7"
     parquet_filename = "train.parquet"
+    save_directory = "scenery"
 
 
 class ActionDatasetInfo(DatasetInfo):
     gdown_object_id = "1QQqBrR3uGDC05Zc11zMeui6Zzl7RvFZg"
     parquet_filename = "train.parquet"
+    save_directory = "action"
 
 
 class EvaluationDatasetInfo(DatasetInfo):
     gdown_object_id = "1oA7W8-Ej_uJEuUxZIjPP5K8hQGGzYsPq"
     parquet_filename = "val.parquet"
+    save_directory = "evaluation"
 
 
 class DatasetType(Enum):
@@ -39,7 +44,7 @@ class LingoQADataset(Dataset):
 
     def __init__(self, type: DatasetType) -> None:
         self.lingoqa_dataset_root_dir = Path(
-            os.environ.get("LINGOQA_DATASET_ROOT_DIR", "/tmp")
+            os.environ.get("LINGOQA_DATASET_ROOT_DIR", "/tmp/lingoqa_dataset")
         )
         if type == DatasetType.SCENARY:
             self.dataset_info = SceneryDatasetInfo()
@@ -51,6 +56,23 @@ class LingoQADataset(Dataset):
             raise Exception(
                 "Dataset type should be scenary/action/evaluation. \
                     Please check type of the dataset."
+            )
+        self.download()
+
+    def dataset_downloaded(self) -> bool:
+        return Path.exists(
+            self.lingoqa_dataset_root_dir.joinpath(self.dataset_info.zip_filename)
+        ) and Path.exists(
+            self.lingoqa_dataset_root_dir.joinpath(self.dataset_info.parquet_filename)
+        )
+
+    def download(self):
+        if not self.dataset_downloaded():
+            os.makedirs(self.lingoqa_dataset_root_dir, exist_ok=True)
+            gdown.download_folder(
+                id=self.dataset_info.gdown_object_id,
+                quiet=False,
+                output=str(self.lingoqa_dataset_root_dir),
             )
 
 
